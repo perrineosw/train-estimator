e7390810bec964d080b3b77bfb5436e0da613681
Ambona Sebastien <sebastien.ambona@agence-gw.com>
2024-02-23 15:36:24 +0100

    WIP : test

--- train-estimator-main/ts/src/train-estimator.spec.ts
+++ train-estimator-main/ts/src/train-estimator.spec.ts
@@ -24,6 +24,16 @@ describe('TrainTicketEstimator', () => {
         await expect(estimator.estimate(tripRequest)).resolves.toBe(0);
     });
 
+   it('devrait lancer une exception InvalidTripInputException pour une ville de arrivé invalide', async () => {
+        const tripRequest = new TripRequest(new TripDetails('Paris', '', new Date()), [new Passenger(25, [])]);
+        await expect(estimator.estimate(tripRequest)).rejects.toThrow("Destination city is invalid");
+   });
+
+   it('Verifie la date', async () => {
+        const tripRequest = new TripRequest(new TripDetails('Paris', 'Lyon', new Date('2021-01-01')), [new Passenger(25, [])]);
+        await expect(estimator.estimate(tripRequest)).rejects.toThrow("Date is invalid");
+   });
+
     it('devrait lancer une exception InvalidTripInputException pour une ville de départ invalide', async () => {
         const tripRequest = new TripRequest(new TripDetails('', 'Lyon', new Date()), [new Passenger(25, [])]);
         await expect(estimator.estimate(tripRequest)).rejects.toThrow("Start city is invalid");
